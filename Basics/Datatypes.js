d042988b1c0072c5e9d0d7cecaedf654e2c7151a
Pankaj Rana <49451931+ranapankaj5610@users.noreply.github.com>
2023-12-11 19:56:39 +0000

    Datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+
+let name = "pankaj"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// Prmitive datatypes:- 
+
+   // number => 2 to power 53
+   // bigint
+   // string => ""
+   // boolean => true/false
+   // null => standalone value
+   // undefined => 
+   // symbol => unique   --> used where we want to find the uniquness, used in react , figma mostly
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
