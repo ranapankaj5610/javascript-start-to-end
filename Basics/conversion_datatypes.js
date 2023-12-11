acbedd09ccfa8a9b186df191cb8aea895ce5980f
Pankaj Rana <49451931+ranapankaj5610@users.noreply.github.com>
2023-12-11 20:11:59 +0000

    Datatype Conversion

---
+++ Basics/conversion_datatypes.js
@@ -0,0 +1,32 @@
+let score = "pankaj"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+
+let valueInNumber = Number(score)
+  //console.log(typeof valueInNumber);
+  //console.log(valueInNumber);
+
+// convert to Number:- 
+  // "33" => 33
+  // "33abc" => NaN
+  //  true => 1; false => 0
+
+let isLoggedIn = "pankaj"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "pankaj" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//console.log(Number(undefined)) ---> NaN
