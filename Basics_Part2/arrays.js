146f672f7824184e3a26fe333436ba1b1347f6b5
Pankaj Rana <49451931+ranapankaj5610@users.noreply.github.com>
2023-12-13 17:12:36 +0000

    updated arrays

--- Basics_Part2/arrays.js
+++ Basics_Part2/arrays.js
@@ -37,3 +37,40 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3) //return array from index 1 to 3 but the difference is it manipulate the original array.
 console.log("C ", myArr);
 console.log(myn2);
+
+/**************************************Array Part 2****************************/
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros). //It will take dc_heroes an element and push in array.
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros). //To concatinate two arrays.
+// console.log(allHeros);  
+
+const all_new_heros = [...marvel_heros, ...dc_heros]  //Spread operator take all the values of array
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) //It will return an array with all the elements inside array with single single. means array inside array now convert to one array only.
+
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh")) //return true false is it an array or not.
+console.log(Array.from("Hitesh")) //return an array 
+console.log(Array.from({name: "hitesh"})) // it will return an empty array because we have to mention that which array we need, array of keys or array of values.
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); //return an array of all the passing variables.
+
