7b5133773bea68a07b53604fc7ce2c08931e1c00
Pankaj Rana <49451931+ranapankaj5610@users.noreply.github.com>
2023-12-14 18:50:33 +0000

    loops and map,reduce,filter methods on array

---
+++ Iterables/reduce.js
@@ -0,0 +1,41 @@
+// const myNums = [1, 2, 3]
+
+// // const myTotal = myNums.reduce(function (acc, currval) {
+// //     console.log(`acc: ${acc} and currval: ${currval}`);
+// //     return acc + currval
+// // }, 0)
+
+// const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+// console.log(myTotal);
+
+
+// const shoppingCart = [
+//     {
+//         itemName: "js course",
+//         price: 2999
+//     },
+//     {
+//         itemName: "py course",
+//         price: 999
+//     },
+//     {
+//         itemName: "mobile dev course",
+//         price: 5999
+//     },
+//     {
+//         itemName: "data science course",
+//         price: 12999
+//     },
+// ]
+
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+// console.log(priceToPay);
+
+let sum=0;
+const arr=[1,3,6,3];
+const totalSum= arr.reduce((sum,val)=>{
+  return sum+val;
+},0);
+console.log(totalSum)
